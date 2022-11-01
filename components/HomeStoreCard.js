00aeab32369686e9a2bf2e3e0d851c0849a7b6e5
victor bahia <vicgabriel.bahia@gmail.com>
2022-11-01 17:43:01 -0300

    mudança código

--- components/HomeStoreCard.js
+++ components/HomeStoreCard.js
@@ -2,5 +2,12 @@ import Image from "next/image";
 import { Col } from "react-bootstrap";
 
 export default function HomeStoreCard(props) {
-  return <>HomeStoreCard</>;
+  return (
+    <>
+      <Col className="m-2">
+        <Image src={props.img} height="400px" />
+        <h5>{props.name}</h5>
+      </Col>
+    </>
+  );
 }
